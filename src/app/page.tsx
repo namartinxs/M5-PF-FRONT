fb20f612a167123f01c936bc310cf2a50d9c2601
Pedro Afonso <pedromanuelafonso47@gmail.com>
2025-07-16 22:12:01 +0100

    feat: adicionando o carrosel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import AboutWe from "@/components/aboutWe";
 import MakeDonation from "@/components/makeDonation";
-
+import Carousel from "../components/Carousel"
 
 
 export default function Home() {
   return (
    <>
+   <section className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
+   
+
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Carousel />
+      </main>
+     </section>
     <AboutWe/>
    <MakeDonation/> 
    
@@ -13,3 +20,4 @@ export default function Home() {
   );
   
 }
+
