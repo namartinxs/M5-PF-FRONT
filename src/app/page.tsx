aa7f0d8599701c28a0a01c0c9c2c03fe5125452a
nathalia <nathaliafloresmartins4550@gmail.com>
2025-07-17 00:12:33 -0300

    feat: ui section makeDonation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import AboutWe from "@/components/aboutWe";
+import MakeDonation from "@/components/mekeDonation";
+
 
 
 export default function Home() {
   return (
    <>
    <AboutWe/>
+   <MakeDonation/>
    </>
   );
 }
