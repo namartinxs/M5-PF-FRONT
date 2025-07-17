308daba3b94c2bff0fb1a960b7f67c24410e237e
nathalia <nathaliafloresmartins4550@gmail.com>
2025-07-17 12:41:17 -0300

    Squashed commit of the following:
    commit 5d893c54019fe29a9f3d7af8e0463a9ff08f5610
    Merge: e85cf9c c6b5ee2
    Author: nathalia <nathaliafloresmartins4550@gmail.com>
    Date:   Thu Jul 17 12:37:19 2025 -0300
    
        Merge branch 'Pedro' of https://github.com/namartinxs/M5-PF-FRONT into Pedro
    
    commit e85cf9ce2fe0023be69c0fd0738fc597267ff6e3
    Author: nathalia <nathaliafloresmartins4550@gmail.com>
    Date:   Thu Jul 17 12:10:29 2025 -0300
    
        fix: fix on package
    
    commit e14b49d5e00d2c2daebf8f1a4d7c22c106e08c88
    Author: Pedro Afonso <pedromanuelafonso47@gmail.com>
    Date:   Thu Jul 17 15:20:53 2025 +0100
    
        feat: adicionando o carrosel
    
    commit fb20f612a167123f01c936bc310cf2a50d9c2601
    Author: Pedro Afonso <pedromanuelafonso47@gmail.com>
    Date:   Wed Jul 16 22:12:01 2025 +0100
    
        feat: adicionando o carrosel
    
    commit c6b5ee2a710a11666f83a385128db26134ab4f72
    Author: nathalia <nathaliafloresmartins4550@gmail.com>
    Date:   Thu Jul 17 12:10:29 2025 -0300
    
        fix: fix on package
    
    commit 89b19c9e4f1d6e2064ec93e780dc9127120c96dc
    Merge: 8c9544e 3c3e0e0
    Author: Pedro Afonso <pedromanuelafonso47@gmail.com>
    Date:   Thu Jul 17 15:23:00 2025 +0100
    
        feat: adicionando o carrosel
    
    commit 8c9544ecde30a21dcac60bae6e75011a5ccd0dcb
    Author: Pedro Afonso <pedromanuelafonso47@gmail.com>
    Date:   Thu Jul 17 15:20:53 2025 +0100
    
        feat: adicionando o carrosel
    
    commit 0f8dd2620d0e07a0687d1aa4b99fe5ff748e205e
    Author: Pedro Afonso <pedromanuelafonso47@gmail.com>
    Date:   Wed Jul 16 22:12:01 2025 +0100
    
        feat: adicionando o carrosel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import AboutWe from "@/components/aboutWe";
 import MakeDonation from "@/components/makeDonation";
-
+import Carousel from "../components/Carousel"
 
 
 export default function Home() {
   return (
-   <>
+    <>
+    <Carousel />
     <AboutWe/>
-   <MakeDonation/> 
-   
-   </>
+   <MakeDonation/>
+   </> 
   );
   
 }
