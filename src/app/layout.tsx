3c3e0e04f6ecd21403683670a8b0711d3c2c190f
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2025-07-17 02:56:57 -0300

    Nathalia (#3)
    * :construction: build: i created the component cardlist
    
    * feat: adição da ui do header
    
    * fix: hide footer and header components in login and cadastro routes
    
    * feat: now header component have a image for home route
    
    * refactor: standardizes colors in header and footer
    
    * refactor: standardized colors
    
    * build: add images used on home
    
    * feat: add section aboutwe
    
    * add section aboutwe on home
    
    * feat: ui section makeDonation
    
    * feat: add redirect button on makeDonatio section
    
    * feat: add outline route button
    
    * feat: adição da ui do header
    
    * fix: hide footer and header components in login and cadastro routes
    
    * feat: now header component have a image for home route
    
    * refactor: standardizes colors in header and footer
    
    * refactor: standardized colors
    
    * build: add images used on home
    
    * feat: add section aboutwe
    
    * add section aboutwe on home
    
    * feat: ui section makeDonation
    
    * feat: add redirect button on makeDonatio section
    
    * feat: add outline route button
    
    * build: default image for cards
    
    * chore: created route for campanhas and turned the cardlists into a component
    
    * refactor: cardlist -> campanhacardlist
    
    * feat: add not found component
    
    * style: now route campanhas is using oops component
    
    * fix: correct the route on 'faça uma doação' button
    
    * fix: fix the component name
    
    * fix: fix the redirect on header
    
    * feat: add Suporte route
    
    ---------
    
    Co-authored-by: ThalitaSamara <thalitasamara61@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ClientLayoutWrapper from "@/components/ClientLayoutWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +23,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientLayoutWrapper>
+          <main className="min-h-screen">{children}</main>
+        </ClientLayoutWrapper>
       </body>
     </html>
   );
